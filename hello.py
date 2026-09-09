75a7ffc226a00c3ea29af9f70864e4292b822350
mbszmn <2605786@sit.singaporetech.edu.sg>
2026-09-09 22:11:40 +0800

    First Post

---
+++ hello.py
@@ -0,0 +1,6 @@
+# Activity 1: Create your First Python Program and add it to the repository
+## Write a welcome message
+print("===================================")
+print("Welcome here")
+print("My first post!")
+print("===================================")
