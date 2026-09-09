5ee71f02a96b657df5f1b606787f1e33fb9d8932
mbszmn <2605786@sit.singaporetech.edu.sg>
2026-09-09 22:18:56 +0800

    Dynamic Profile

--- hello.py
+++ hello.py
@@ -11,18 +11,32 @@ username = "cool_creator" # store username as a string in your variable
 bio = "Fun Blogger"       # store bio as a string in your variable
 followers = 100           # store followers as an integer in your variable
 
-print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+#print("Username:", username)
+#print("Bio:", bio)
+#print("Followers:", followers)
 
 # Activity 3: Follower Growth tracker
 followers = 100
 
 followers += 50
-print("Day 1:", followers)
+#print("Day 1:", followers)
 
 followers += 20
-print("Day 2:", followers)
+#print("Day 2:", followers)
 
 followers -= 10
-print("Day 3:", followers)
+#print("Day 3:", followers)
+
+
+# Activity 4: Interactive profile creator
+## Currently the program displays hard coded values. Let’s make it more interactive and personalized
+
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
