82655d8e13b3dfbc530f28448af9c5ebc5d67269
mbszmn <2605786@sit.singaporetech.edu.sg>
2026-09-09 22:12:59 +0800

    Added Profile Variables

--- hello.py
+++ hello.py
@@ -4,3 +4,13 @@ print("===================================")
 print("Welcome here")
 print("My first post!")
 print("===================================")
+
+# Activity 2: Update your Profile and add it to the repository
+## Create an Instagram profile
+username = "cool_creator" # store username as a string in your variable
+bio = "Fun Blogger"       # store bio as a string in your variable
+followers = 100           # store followers as an integer in your variable
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
